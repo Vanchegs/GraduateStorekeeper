e68cd5803f874ae20d63a0b1e96c35b66827eb1d
Vanchegs <manshin9300@gmail.com>
2025-01-13 06:34:15 +0300

    minFix camera limit

---
+++ Assets/Internal/Codebase/Camera/CameraMotionLimitation.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Internal.Codebase.Camera
+{
+    public class CameraMotionLimitation : MonoBehaviour
+    {
+        [SerializeField] private float leftLimitX, rightLimitX;
+        [SerializeField] private float leftLimitY, rightLimitY;
+        
+        
+    }
+}
