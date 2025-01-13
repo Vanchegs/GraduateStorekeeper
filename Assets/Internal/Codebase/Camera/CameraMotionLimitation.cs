d68f741ef55787f083597c6fb68b8236854e1ee0
Vanchegs <manshin9300@gmail.com>
2025-01-13 15:37:32 +0300

    minFix

--- Assets/Internal/Codebase/Camera/CameraMotionLimitation.cs
+++ Assets/Internal/Codebase/Camera/CameraMotionLimitation.cs
@@ -4,9 +4,17 @@ namespace Internal.Codebase.Camera
 {
     public class CameraMotionLimitation : MonoBehaviour
     {
-        [SerializeField] private float leftLimitX, rightLimitX;
-        [SerializeField] private float leftLimitY, rightLimitY;
-        
-        
+        [SerializeField] private float minValueX, maxValueX; 
+        [SerializeField] private float minValueY, maxValueY;
+        [SerializeField] private Transform cameraTransform; 
+
+        void Update()
+        {
+            float clampedX = Mathf.Clamp(cameraTransform.position.x, minValueX, maxValueX);
+            
+            float clampedY = Mathf.Clamp(cameraTransform.position.y, minValueY, maxValueY);
+            
+            cameraTransform.position = new Vector2(clampedX, clampedY);
+        }
     }
 }
