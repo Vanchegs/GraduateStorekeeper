c07d431b5d7c1bc890a62f9ed9a5a2a53eec46eb
Vanchegs <manshin9300@gmail.com>
2025-01-14 05:50:48 +0300

    issue update

--- Assets/Internal/Codebase/Camera/CameraMotionLimitation.cs
+++ Assets/Internal/Codebase/Camera/CameraMotionLimitation.cs
@@ -1,20 +1,27 @@
 using UnityEngine;
 
-namespace Internal.Codebase.Camera
+namespace Internal.Codebase
 {
     public class CameraMotionLimitation : MonoBehaviour
     {
         [SerializeField] private float minValueX, maxValueX; 
         [SerializeField] private float minValueY, maxValueY;
-        [SerializeField] private Transform cameraTransform; 
+        
+        private Transform cameraTransform;
 
-        void Update()
+        private void Start() => 
+            cameraTransform = GetComponent<Transform>();
+
+        private void LateUpdate() => 
+            CheckCameraLimit();
+
+        private void CheckCameraLimit()
         {
             float clampedX = Mathf.Clamp(cameraTransform.position.x, minValueX, maxValueX);
-            
+
             float clampedY = Mathf.Clamp(cameraTransform.position.y, minValueY, maxValueY);
-            
-            cameraTransform.position = new Vector2(clampedX, clampedY);
+
+            cameraTransform.position = new Vector3(clampedX, clampedY, cameraTransform.position.z);
         }
     }
 }
